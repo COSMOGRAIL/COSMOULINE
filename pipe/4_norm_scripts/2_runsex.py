969411672c8fee7b1fc1c8c26cfe70a308065be6
voeroes <voeroes@6d28bc59-9848-486d-8bae-1920113d1a71>
2010-10-25 09:22:14 +0000

    add the automatic creation of the sextracor input "default_key.sex". Same mecanism as for 1_character_scripts/3a_runsex.py

--- pipe/4_norm_scripts/2_runsex.py
+++ pipe/4_norm_scripts/2_runsex.py
@@ -2,19 +2,21 @@
 #	We run sextractor on the aligned images
 #
 
+
 execfile("../config.py")
 from kirbybase import KirbyBase, KBError
 from variousfct import *
 from datetime import datetime, timedelta
+from readandreplace_fct import *
+import shutil
+import os
 
-
-print "Hola amigo : don't forget to check the sextractor configuration !"
-print "Put the pixel size of the reference frame."
+print "Note that the program set itself the gain and the pixel scale in the default_sky.sex according to the database. Great!"
 proquest(askquestions)
 
 	# select images to treat
 db = KirbyBase()
-images = db.select(imgdb, ['gogogo', 'treatme'], [True, True], ['recno','imgname'], returnType='dict')
+images = db.select(imgdb, ['gogogo', 'treatme'], [True, True], ['recno','imgname','pixsize', 'gain'], returnType='dict')
 
 nbrofimages = len(images)
 print "I have", nbrofimages, "images to treat."
@@ -23,6 +25,16 @@ proquest(askquestions)
 
 starttime = datetime.now()
 
+# default parameter for the sextractor input
+
+scriptdir = os.path.join(pipedir, "4_norm_scripts")
+key = "norm"	# key is a key word according to what you're doing with sextractor (mesuring seeing: key = see)
+default_template_filename = os.path.join(scriptdir, "default_" +key+ "_template.sex")
+pixesize = 0.0	
+gain = 0.0
+
+sexin = "default_" +key+ ".sex" 	#name of the sextractor input file
+
 for i, image in enumerate(images):
 
 	print " -" * 20
@@ -30,11 +42,31 @@ for i, image in enumerate(images):
 	img = alidir + image['imgname'] + "_ali.fits"
 	sexcat = alidir + image['imgname'] + ".alicat"
 	
-	sexout = os.system(sex +" "+ img + " -c default_norm.sex")
+	if pixesize != image["pixsize"] or gain != image["gain"]:
+		# I write default_sky.sex
+		
+		pixesize = image["pixsize"]
+		gain = image["gain"]
+	
+		default_sky_template = justread(default_template_filename)
+		defaultdict = {"$gain$": str(image["gain"]), "$px_size$": str(image["pixsize"])}
+		defaultsex = justreplace(default_sky_template, defaultdict)
+		defaultfile = open(os.path.join(scriptdir, sexin), "w")	 
+		defaultfile.write(defaultsex)
+		defaultfile.close()
+		
+		print "Wrote default_sky.sex"
+	
+	
+	sexout = os.system(sex +" "+ img + " -c " +sexin)
+	
 	os.system("mv sex.cat " + sexcat)
 	#os.system("rm check.fits")
 
-	
+
+#We delete the default_sky.sex to keep the directory clean
+os.remove(os.path.join(scriptdir, sexin))
+
 #db.pack(imgdb) # to erase the blank lines
 
 endtime = datetime.now()
