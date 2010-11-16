496d9ed194356858ec6c12aef7d0d37f580104ca
cataldi <cataldi@6d28bc59-9848-486d-8bae-1920113d1a71>
2010-11-16 14:36:02 +0000

    added a question at the beginning of the script

--- skysim/pipe/1_createsimimg_example.py
+++ skysim/pipe/1_createsimimg_example.py
@@ -8,6 +8,8 @@ from variousfct import *
 import numpy as np
 
 
+print 'Hello skywalker! So you want to create instances of the simimg class called %s, is this true?' %(simname)
+proquest(askquestions)
 
 
 
@@ -16,7 +18,7 @@ catalogue=[]
 
 
 #lets create images with one star, the seeing changing regularly from img to img
-seeing_space=np.linspace(0.25,3.5,250)     #minimal seeing, maximal seeing, nb of images
+seeing_space=np.linspace(0.45,3.7,250)     #minimal seeing, maximal seeing, nb of images
 
 for seeingvalue in seeing_space: 
     thissimimg = simimg.simimg(image_size = (400,400), seeing_fwhm = seeingvalue,  sky_list = skylist_singlestar(x=200,y=200,mag=20))
@@ -27,5 +29,6 @@ for seeingvalue in seeing_space:
 
 #we write the catalogue into a pickle
 writepickle(catalogue, os.path.join(pipedir, simname + '.pkl'))
+
 
 
