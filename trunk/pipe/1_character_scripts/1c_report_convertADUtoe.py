0daad23651ee7894c4a60793f12cd43a7d7cca72
voeroes <voeroes@6d28bc59-9848-486d-8bae-1920113d1a71>
2010-11-19 13:49:09 +0000

    updated the dsiplay

--- trunk/pipe/1_character_scripts/1c_report_convertADUtoe.py
+++ trunk/pipe/1_character_scripts/1c_report_convertADUtoe.py
@@ -1,3 +1,4 @@
+
 #
 #	write a report about the convertion of the image from ADU to electron
 #
@@ -12,8 +13,14 @@ fields = ['imgname','gain', 'origin_gain']
 db = KirbyBase()
 reporttxt = ""
 
-setreport = db.select(imgdb, ['recno'], ['*'], fields, returnType='report')
-reporttxt += setreport
+usedsetnames = set(map(lambda x : x[0], db.select(imgdb, ['recno'], ['*'], ['setname'])))
+
+for setname in usedsetnames:
+	
+	reporttxt += "\n\n" + 20*"=" +  "%10s "%setname + 20*"=" +"\n\n"
+
+	setreport = db.select(imgdb, ['setname'], [setname], fields, returnType='report')
+	reporttxt += setreport
 
 reporttxtfile = open(os.path.join(workdir, "report_convert.txt"), "w")
 reporttxtfile.write(reporttxt)
