c6c7d188ce0d3519ed1b23acd71ff31832a7716e
voeroes <voeroes@6d28bc59-9848-486d-8bae-1920113d1a71>
2010-11-19 12:42:02 +0000

    added stuff to support the combination mecanism

--- trunk/pipe/1_character_scripts/3a_runsex.py
+++ trunk/pipe/1_character_scripts/3a_runsex.py
@@ -12,7 +12,7 @@ import shutil
 import os
 
 
-print "Note that the program sets itself the gain and the pixel scale in the default_see.sex according to the database. Great!"
+print "Note that the program sets itself the gain, the satur level and the pixel scale in the default_see.sex according to the database. Great!"
 # The following line will interactively ask if you want to go on or abort.
 # "askquestions" can be set to False in config.py, in which case we skip this. 
 proquest(askquestions)
@@ -34,6 +34,8 @@ key = "see"	# key is a key word according to what you're doing with sextractor (
 default_template_filename = os.path.join(scriptdir, "default_" +key+ "_template.sex")
 pixesize = 0.0	
 gain = 0.0
+satur_level = 0.0
+satur_levelADU = 65000.0
 
 sexin = "default_" +key+ ".sex" 	#name of the sextractor input file
 
@@ -43,20 +45,24 @@ for i,image in enumerate(images):
 	print "- " * 30
 	print i+1, "/", nbrofimages, ":", image['imgname']
 	
-	if pixesize != image["pixsize"] or gain != image["gain"]:
-		# I write default_sky.sex
+	# I write default_sky.sex
 		
-		pixesize = image["pixsize"]
-		gain = image["gain"]
+	pixesize = image["pixsize"]
+	gain = image["gain"]
+		
+	if image[combinumname] < 0.0:		#in this case, we have a combine image and therefore the satur_level must be multiply by the number of combined images
+		satur_level = image["origin_gain"]*satur_levelADU*image["preredfloat1"]
+	else:
+		satur_level = image["origin_gain"]*satur_levelADU
 	
-		default_sky_template = justread(default_template_filename)
-		defaultdict = {"$gain$": str(image["gain"]), "$px_size$": str(image["pixsize"])}
-		defaultsex = justreplace(default_sky_template, defaultdict)
-		defaultfile = open(os.path.join(scriptdir, sexin), "w")	 
-		defaultfile.write(defaultsex)
-		defaultfile.close()
+	default_sky_template = justread(default_template_filename)
+	defaultdict = {"$gain$": str(image["gain"]), "$px_size$": str(image["pixsize"]), "$satur_level$": str(satur_level)}
+	defaultsex = justreplace(default_sky_template, defaultdict)
+	defaultfile = open(os.path.join(scriptdir, sexin), "w")	 
+	defaultfile.write(defaultsex)
+	defaultfile.close()
 		
-		print "Wrote default_sky.sex"
+	print "Wrote default_sky.sex"
 	
 	
 	sexout = os.system(sex +" "+ image['rawimg'] + " -c " +sexin)
@@ -65,16 +71,6 @@ for i,image in enumerate(images):
 	os.system("mv sex.cat "+catfilename)
 	
 	
-	# for convenience, we make an alias to the original file
-	newfilepath = alidir + image['imgname'] + ".fits"
-	
-	# the old way was :
-	#if os.path.islink(newfilepath):
-	#	os.remove(newfilepath)
-	#os.symlink(image['rawimg'], newfilepath)
-	
-	#The new way :
-	copyorlink(image['rawimg'], newfilepath, uselinks)
 	
 	# remove check image in case it was created
 	if os.path.isfile("check.fits"):
