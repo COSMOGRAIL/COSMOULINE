aff04232f30ca71cc00086350c37d5a11225019b
mtewes <mtewes@6d28bc59-9848-486d-8bae-1920113d1a71>
2010-11-29 10:50:49 +0000

    Update for new star class + sex photom + raw mcs fluxes in db (no longer normalized).

---
+++ trunk/pipe/2_skysub_scripts/2_skypng.py
@@ -0,0 +1,106 @@
+#
+#	Control png of the image and sky
+#
+
+execfile("../config.py")
+from kirbybase import KirbyBase, KBError
+from variousfct import *
+from datetime import datetime, timedelta
+import shutil
+import os
+import f2n
+import star
+
+
+db = KirbyBase()
+if thisisatest:
+	print "This is a test run."
+	images = db.select(imgdb, ['gogogo','treatme','testlist'], [True, True, True], returnType='dict', sortFields=['setname','mjd'])
+else:
+	images = db.select(imgdb, ['gogogo','treatme'], [True, True], returnType='dict', sortFields=['setname','mjd'])
+	
+	
+nbrimages = len(images)
+print "Number of images to treat :", nbrimages
+proquest(askquestions)
+
+
+pngdirpath = os.path.join(workdir, "sky_png")
+if os.path.isdir(pngdirpath):
+	print "I will delete existing stuff."
+	proquest(askquestions)
+	shutil.rmtree(pngdirpath)
+os.mkdir(pngdirpath)
+
+
+starttime = datetime.now()
+
+for i,image in enumerate(images):
+	
+	print "+ " * 30
+	print "%5i/%i : %s" % (i+1, nbrimages, image["imgname"])
+	
+	
+	skyimagepath = os.path.join(alidir,  image["imgname"] + "_sky.fits")
+	skysubimagepath = os.path.join(alidir, image["imgname"] + "_skysub.fits")
+	
+	
+	
+	skyimage = f2n.fromfits(skyimagepath)
+	skyimage.setzscale("ex", "ex")
+	skyimage.rebin(3)
+	skyimage.makepilimage(scale = "lin", negative = False)
+	skyimage.writetitle("Sky", colour=(255, 0, 0))
+	
+	skysubimage = f2n.fromfits(skysubimagepath)
+	skysubimage.setzscale("auto", "auto")
+	skysubimage.rebin(3)
+	skysubimage.makepilimage(scale = "log", negative = False)
+	skysubimage.writetitle("Skysubtracted image")
+	
+	# We read the 20 strongest stars from sextractor :
+	sexcatpath = os.path.join(alidir, image['imgname'] + ".cat")
+	sexcat = star.readsexcat(sexcatpath)
+	sexcat = star.sortstarlistbyflux(sexcat)
+	sexcatasdicts = [{"name":s.name, "x":s.x, "y":s.y} for s in sexcat[:20]]
+	
+	skysubimage.drawstarslist(sexcatasdicts, r = 30, colour = (255, 255, 255))
+	skyimage.drawstarslist(sexcatasdicts, r = 30, colour = (255, 0, 0))
+
+	skysubinfo = [
+	"%s" % image["imgname"],
+	"%s UTC" % image['datet'],
+	image['telescopename'] + " - " + image['setname'],
+	"Seeing : %4.2f [arcsec]" % image['seeing'],
+	"Ellipticity : %4.2f" % image['ell'],
+	"Airmass : %4.2f" % image['airmass'],
+	"Sky level : %.1f" % image['skylevel'],
+	"Sky stddev : %.1f" % image['prealistddev'],
+	]
+	skysubimage.writeinfo(skysubinfo, colour = (255, 255, 255))
+	
+	skyinfo = [
+	"%s : %i -> %i (span = %i) [ADU]" % ("Cuts", skyimage.z1, skyimage.z2, skyimage.z2 - skyimage.z1)
+	]
+	skyimage.writeinfo(skyinfo, colour = (255, 0, 0))
+	
+	
+
+	
+	pngpath = os.path.join(pngdirpath, "%s_sky.png" % image['imgname'])
+	f2n.compose([[skysubimage, skyimage]], pngpath)
+
+	orderlink = os.path.join(pngdirpath, "%05i.png" % (i+1)) # a link to get the images sorted for the movies etc.
+	os.symlink(pngpath, orderlink)
+	
+
+
+endtime = datetime.now()
+timetaken = nicetimediff(endtime - starttime)
+notify(computer, withsound, "PNGs written in %s I took %s" % (pngdirpath, timetaken))
+
+if makejpgarchives :
+	makejpgtgz(pngdirpath, workdir, askquestions = askquestions)
+
+
+
---
+++ trunk/pipe/4_norm_scripts/2b_facult_photomdb.py
@@ -0,0 +1,99 @@
+"""
+We add raw sextractor photom into the database (i.e., not normalized).
+Can be run at any time once :
+	- the alignment is done
+	- the normalisation catalogs are written (2_runsex.py)
+	
+"""
+
+###########################################################################
+# Fields you want in the database. Not restricted to photometry, you could add any fields.
+# Note the convention : if you ask sextractor for 3 apertures, those fields will be named
+# FLUX_APER, FLUX_APER1, FLUX_APER2 (this is done by astroasciidata, not by sextractor)
+# Do not forget to update both default.sex and default.param if you want to make changes to
+# the apertures.
+
+sexphotomname = "sexphotom1"
+
+sexphotomfields = [
+{"sexname":"FLUX_AUTO", "dbname":"auto_flux", "type":"float"},
+{"sexname":"FLUX_APER", "dbname":"ap20_flux", "type":"float"},
+{"sexname":"FLUX_APER1", "dbname":"ap30_flux", "type":"float"},
+{"sexname":"FLUX_APER2", "dbname":"ap60_flux", "type":"float"},
+{"sexname":"FLUX_APER3", "dbname":"ap90_flux", "type":"float"},
+{"sexname":"FLUX_APER4", "dbname":"ap120_flux", "type":"float"}
+]
+
+###########################################################################
+
+
+execfile("../config.py")
+from kirbybase import KirbyBase, KBError
+from variousfct import *
+import star
+
+# Selecting the images
+db = KirbyBase()
+images = db.select(imgdb, ['gogogo', 'treatme'], [True, True], returnType='dict')
+nbrofimages = len(images)
+
+# Read the manual star catalog :
+photomstarscatpath = os.path.join(configdir, "photomstars.cat")
+photomstars = star.readmancat(photomstarscatpath)
+print "I will look for these stars :"
+print "\n".join(["%s\t%.2f\t%.2f" % (s.name, s.x, s.y) for s in photomstars])
+
+print "This will require the following fields :"
+# Fields for the database :
+dbfields = [{"name":"%s_%s_%s" % (sexphotomname, s.name, f["dbname"]), "type":f["type"]} for s in photomstars for f in sexphotomfields]
+print "\n".join(["%30s : %5s" % (f["name"], f["type"]) for f in dbfields])
+
+
+dbfieldstoadd = []
+for f in dbfields:
+	if f["name"] not in db.getFieldNames(imgdb) :
+		dbfieldstoadd.append(f)
+print "I will add %i/%i fields to the database." % (len(dbfieldstoadd), len(dbfields))
+
+proquest(askquestions)
+
+# As we will tweak the database, backup
+backupfile(imgdb, dbbudir, 'calccoeff')
+	
+db.addFields(imgdb, ["%s:%s" % (f["name"], f["type"]) for f in dbfieldstoadd])
+
+
+for i, image in enumerate(images):
+	print "- "*30
+	print i+1, "/", nbrofimages, ":", image['imgname']
+	
+	# We read the sextractor catalog :
+	sexcatpath = os.path.join(alidir, image['imgname'] + ".alicat")
+	sexstars = star.readsexcat(sexcatpath, maxflag = 0, posflux = False, propfields=[f["sexname"] for f in sexphotomfields])
+		
+	# We identify these sextractor stars with the handwritten selection
+	id = star.listidentify(photomstars, sexstars, tolerance = identtolerance, onlysingle = True, verbose = True)
+	
+	# This is already printed by listidentify
+	#if len(id["nomatchnames"]) > 0:
+	#	print "No match for " + ", ".join([n for n in id["nomatchnames"]])
+	#if len(id["notsurenames"]) > 0:
+	#	print "Not sure for " + ", ".join([n for n in id["notsurenames"]])
+	
+	if len(id["match"]) == 0:
+		print "No stars identified, skipping this image."
+		continue
+	
+	updatefieldnames = []
+	updatefieldvalues = []
+	for s in id["match"]:
+		for f in sexphotomfields:
+			updatefieldnames.append("%s_%s_%s" % (sexphotomname, s.name, f["dbname"]))
+			updatefieldvalues.append(s.props[f["sexname"]])
+		
+
+	db.update(imgdb, ['recno'], [image["recno"]], updatefieldvalues, updatefieldnames)
+
+db.pack(imgdb) # to erase the blank lines
+
+print "Done."
