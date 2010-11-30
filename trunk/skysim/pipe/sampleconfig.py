12a399d01565db6a039ba08052f7dc5e913ac5ca
cataldi <cataldi@6d28bc59-9848-486d-8bae-1920113d1a71>
2010-11-30 13:18:53 +0000

    removed default values

--- trunk/skysim/pipe/sampleconfig.py
+++ trunk/skysim/pipe/sampleconfig.py
@@ -4,19 +4,6 @@ import os
 import numpy as np
 
 
-#default parameters
-
-gain = 1.0
-detector_effic = 1.0	#quantum efficiency of the detector
-exptime = 300.0
-pixel_size = 0.2	#in arcsecond
-telescope_diam = 3.6
-
-sky_level = 1000.0	#sky level in ADU
-
-back_mag = -2.5*np.log10((4.0 *sky_level * gain * detector_effic)/(exptime * np.pi * telescope_diam**2)) / pixel_size**2	# background surface brightness (mag/arsec2)
-
-
 #the path of the skymaker software
 sky = '/home/epfl/tewes/bin/skymaker-3.3.3/src/sky'
 
