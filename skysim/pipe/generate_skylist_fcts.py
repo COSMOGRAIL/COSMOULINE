d8810aa0de7fb00391eec1bb3f60c790bba0ed34
cataldi <cataldi@6d28bc59-9848-486d-8bae-1920113d1a71>
2010-12-02 12:33:43 +0000

    adjusted the shift/rotation functions to the new star class

--- skysim/pipe/generate_skylist_fcts.py
+++ skysim/pipe/generate_skylist_fcts.py
@@ -2,8 +2,7 @@
 
 import sys
 import random
-import math
-import cmath
+import copy
 from star import *
 
 
@@ -47,10 +46,6 @@ def skylist_starnetwork(imgdimx, imgdimy, matrix_x, matrix_y, distance=None, mag
         dist_y=effective_y / float(matrix_y + 1)
 
 
-        #fill in the list
-
-
-
         # we iterate in the following way over the matrix (example with matrix_x=3, matrix_y=2):
         #1 3 5
         #0 2 4
@@ -59,7 +54,7 @@ def skylist_starnetwork(imgdimx, imgdimy, matrix_x, matrix_y, distance=None, mag
             for j in range(matrix_y):
                 x = xborder + (i+1)*dist_x
                 y = yborder + (j+1)*dist_y
-		starlist.append(Star(pos_x = x, pos_y = y, star_mag = mag))
+		starlist.append(Star(pos_x = x, pos_y = y, mag = mag))
 
 
 
@@ -78,11 +73,11 @@ def skylist_starnetwork(imgdimx, imgdimy, matrix_x, matrix_y, distance=None, mag
             for j in range(matrix_y):
                 x = start_x + i*distance
                 y = start_y + j*distance
-		starlist.append(Star(pos_x = x, pos_y = y, star_mag = mag))
+		starlist.append(Star(pos_x = x, pos_y = y, mag = mag))
 
 
 
-    return starlist                        #a list python
+    return starlist                        #a list of instances of the class star
 
 
 
@@ -113,80 +108,55 @@ def skylist_randomstars(imgdimx, imgdimy, nb_stars = 0, mag_min = 17.0, mag_max 
 		# we set randomly the magnitude between a max and a min value
 		mag = random.uniform(mag_min, mag_max) 
 
-		starlist.append(Star(pos_x = x, pos_y = y, star_mag = mag))
+		starlist.append(Star(pos_x = x, pos_y = y, mag = mag))
 
-	return starlist		#a list python
+	return starlist		
+
+
 
 
 #####################################################################################################################
-#a fct that rotates and applies a shift to a starlist
+#a fct that takes an input list and return a list with all the stars shifted and rotated
 #the angle is in degrees, the shift in pixels
 
 def rot_shift_starlist(inputlist, shiftx, shifty, angle):
 
     #we create a deep copy of the input starlist:
-    outputlist=asciidata.create(len(inputlist), inputlist.nrows)
-    for index in range(len(inputlist)):
-        outputlist[index] = inputlist[index].copy()
-
-
-    #we use complex numbers to generate to coord transformation
-    shift = complex(shiftx, shifty)
-    rotation = cmath.exp(math.radians(angle)*1j)
+    outputlist = copy.deepcopy(inputlist)
     
 
     #we apply the transformation
-    for index in range(outputlist.nrows):
-        inputcoords =  complex(outputlist['x'][index], outputlist['y'][index])
-        outputcoords = rotation * inputcoords + shift                           #the transformed coords in complex notation
-        outputlist['x'][index] = outputcoords.real
-        outputlist['y'][index] = outputcoords.imag
-
+    for Star in outputlist:
+        Star.shift(shiftx, shifty)
+        Star.rotate(angle)
 
     return outputlist
 
 
 
+
+
+
 ######################################################################################################################
+#a fct that applies a random shift/rotation to a starlist, where the shift is contained in [-shift_max, shift_max] and the angle is contained in [-angle_max, angle_max]
+#Units: shift [pixel], angle [degrees]
 
-#a fct that takes a starlist (asciidata object) as input and returns a couple of new starlists rotated and shifted with respect to the input starlist
-#the fct will simply modify the coordinates of the stars in the input starlist
-#nb_starlist is the number of starlists you want to create
-#shift_max is in pixels, angle_max in degrees; they give the maximal value of the absolute value of the random shift/angle we apply to the input starlist
-#(i.e. if angle_max=10, there will be rotations between -10 and 10 degrees)
-
-
-def generate_rot_shift_starlists(inputlist, nb_starlists, shiftx_max, shifty_max, angle_max):
-
-    print 'I will apply coordinate transformations with the following properties:'
+def random_rot_shift_starlist(inputlist, shiftx_max, shifty_max, angle_max):
+    print 'I will apply a coordinate transformation with the following properties:'
     print 'Shift in x direction between %.2f and %.2f pixels.' %(-abs(shiftx_max), abs(shiftx_max))
     print 'Shift in y direction between %.2f and %.2f pixels.' %(-abs(shifty_max), abs(shifty_max))
-    print 'Rotation between %.4f and %.4f degrees.' %(-abs(angle_max), abs(angle_max))
+    print 'Rotation between %.4f and %.4f degrees.' %(-abs(angle_max), abs(angle_max))   
+
+    shiftx = random.uniform( - shiftx_max, shiftx_max)     #no problem if -shiftx_max > shiftx_max
+    shifty = random.uniform( - shifty_max, shifty_max)
+    angle = random.uniform( - angle_max, angle_max)
+
+    return rot_shift_starlist(inputlist, shiftx, shifty, angle)
 
 
-    starlists=[]
 
 
 
-    #Now we create the starlists
-    for i in range(nb_starlists):
-        print 'Generating transformed starlist ', i+1, "/", nb_starlists
-
-
-        #we calculat the shift and the rotation angle for the starlist to generate
-
-        shiftx = random.uniform( - shiftx_max, shiftx_max)     #no problem if -shiftx_max > shiftx_max
-        shifty = random.uniform( - shifty_max, shifty_max)
-        angle = random.uniform( - angle_max, angle_max)
-        
-
-        transf_list = rot_shift_starlist(inputlist, shiftx, shifty, angle)
-
-
-        starlists.append(transf_list)
-
-
-    return starlists                                    #a list of asciidata objects
 
 
 
