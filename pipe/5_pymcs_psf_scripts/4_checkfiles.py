748dfe7e28ae8d26efecf409e2a0b48d7296bb98
mtewes <mtewes@users.noreply.github.com>
2011-01-21 15:02:36 +0000

    Solving problems for png creation with broken pyMCS psfs

--- pipe/5_pymcs_psf_scripts/4_checkfiles.py
+++ pipe/5_pymcs_psf_scripts/4_checkfiles.py
@@ -12,6 +12,9 @@ db = KirbyBase()
 images = db.select(imgdb, ['gogogo','treatme',psfkeyflag], [True,True,True], returnType='dict', sortFields=['mjd'])
 
 
+
+print "Images that you should put on the psfskiplist now :"
+
 for i, image in enumerate(images):
 
 	imgpsfdir = os.path.join(psfdir, image['imgname'])
