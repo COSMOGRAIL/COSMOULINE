8d2b7f4ccf9bf3fa58f44018f1ad7789efa93516
mtewes <mtewes@6d28bc59-9848-486d-8bae-1920113d1a71>
2011-01-28 16:08:32 +0000

    added rm electron images

---
+++ trunk/pipe/3_align_scripts/5b_facult_rm_electrons.py
@@ -0,0 +1,30 @@
+
+execfile("../config.py")
+from kirbybase import KirbyBase, KBError
+from variousfct import *
+import glob
+
+# We select the images to treat :
+db = KirbyBase()
+
+print 'I will simply remove all the orignial images (converted to electrons and hence copied) from the alidir.'
+print 'This will save some disk space, now that images are sky subtracted...'
+print "Soft links would not be removed."
+
+db = KirbyBase()
+images = db.select(imgdb, ['gogogo','treatme'], [True, True], returnType='dict')
+
+print "I will try to delete %i images." % len(images)
+proquest(askquestions)
+for i, image in enumerate(images):
+
+	print "%i : %s" % (i+1, image["imgname"])
+	filepath = os.path.join(alidir, image["imgname"] + ".fits")
+	if os.path.isfile(filepath):
+		os.remove(filepath)
+	else:
+		print "Hmm, no such file."
+	
+print "Done."
+	
+
