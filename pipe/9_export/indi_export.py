abd7cf21e4191e8a6178351d3fa4105984823f1d
mtewes <mtewes@users.noreply.github.com>
2011-02-16 15:46:01 +0000

    for old files

--- pipe/9_export/indi_export.py
+++ pipe/9_export/indi_export.py
@@ -5,19 +5,24 @@ import variousfct
 import datetime
 
 
-print "I am the only scirpt that writes into your configdir."
-print "But I will try to be careful."
+print "I am a special version, if you just want to translate an old db into a pkl."
+print "I write my files in the current directory."
+print "You have to configure me..."
 
+
+
+configstr = "RXJ1131"
+databasepath = "/home/epfl/tewes/RXJ1131_database.dat" 
 
 
 ############ Building the filenames ##############
 
 now = datetime.datetime.now()
 datestr = now.strftime("%Y-%m-%d")
-configstr = os.path.split(configdir)[-1]
 
 filename = "%s_%s" % (datestr, configstr)
 
+configdir = "."
 readmefilepath = os.path.join(configdir, filename + "_readme.txt")
 pklfilepath = os.path.join(configdir, filename + "_db.pkl")
 
@@ -31,10 +36,10 @@ readme = ["This is the automatic readme file for\n%s\n" %  pklfilepath]
 
 # We do only one select :
 db = KirbyBase()
-images = db.select(imgdb, ['recno'], ['*'], sortFields=['setname', 'mjd'], returnType='dict')
+images = db.select(databasepath, ['recno'], ['*'], sortFields=['setname', 'mjd'], returnType='dict')
 mjdsortedimages = sorted(images, key=lambda k: k['mjd'])
 
-readme.append("Target : %s" % xephemlens)
+readme.append("Target : %s" % configstr)
 
 readme.append("Total : %i images" % (len(images)))
 readme.append("Time span : %s -> %s" % (mjdsortedimages[0]["datet"], mjdsortedimages[-1]["datet"]))
@@ -46,12 +51,12 @@ readme.append("Telescopes : %s" % ",".join(telescopes))
 readme.append("Setnames : %s" % ",".join(setnames))
 
 
-readme.append("Ref image name : %s " % refimgname)
+#readme.append("Ref image name : %s " % refimgname)
 
 
 
-fieldnames = db.getFieldNames(imgdb)
-fieldtypes = db.getFieldTypes(imgdb)
+fieldnames = db.getFieldNames(databasepath)
+fieldtypes = db.getFieldTypes(databasepath)
 fielddesc = ["%s %s" % (fieldname, fieldtype) for (fieldname, fieldtype) in zip(fieldnames, fieldtypes)]
 
 
@@ -71,42 +76,6 @@ renorms = [fieldname for fieldname in fieldnames if fieldname[0:6] == "renorm"]
 readme.append("\nRenorms :")
 readme.extend(renorms)
 
-
-
-"""
-
-usedsetnames = map(lambda x : x[0], db.select(imgdb, ['recno'], ['*'], ['setname']))
-gogogotrue = map(lambda x : x[0], db.select(imgdb, ['gogogo'], [True], ['setname']))
-usedsetnameshisto = "".join(["%10s : %4i images (%4i with gogogo  == True)\n"%(item, usedsetnames.count(item), gogogotrue.count(item)) for item in sorted(list(set(usedsetnames)))])
-
-print "- "*40
-print "Image sets summary :"
-print usedsetnameshisto.rstrip("\n")
-
-treatmetrue = map(lambda x : x[0], db.select(imgdb, ['treatme'], [True], ['setname']))
-usedsetnameshisto = [(item, usedsetnames.count(item), treatmetrue.count(item)) for item in sorted(list(set(usedsetnames)))]
-
-print "- "*40
-print "treatme flag summary :"
-for item in usedsetnameshisto:
-	if item[1] == item[2]:
-		print "%10s : "%item[0] + "will all be treated (except gogogo == False)."
-	elif item[1] != item[2] and item[2] != 0:
-		print "%10s : "%item[0] + "will be partially treated (", item[2], "among", item[1], ") ???"
-	elif item[2] == 0:
-		print "%10s : "%item[0] + "will be skipped."
-	else :
-		print "I have a big bad problem with", item[0]
-
-print "- "*40
-
-treatmetrue = map(lambda x : x[0], db.select(imgdb, ['treatme','gogogo'], [True, True], ['setname']))
-
-
-print "Number of images to treat that have gogogo == True :", len(treatmetrue)
-print "Total number of images :", len(usedsetnames)
-print "Images with gogogo == True :", len(gogogotrue)
-"""
 
 
 readmetxt = "\n".join(readme)
@@ -137,8 +106,4 @@ out_file.close()
 print "Wrote %s" % readmefilepath
 
 variousfct.writepickle(images, pklfilepath, verbose=True)
-
-
-
-#rdbexport.writerdb(exportcols, "out.rdb", True)
 
