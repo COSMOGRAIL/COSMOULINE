d37b839f64df6ce78e9fa49bd7549eab836d27c0
mtewes <mtewes@users.noreply.github.com>
2011-02-22 15:08:45 +0000

    Updates

--- pipe/4_norm_scripts/2a_runsex_NU.py
+++ pipe/4_norm_scripts/2a_runsex_NU.py
@@ -32,10 +32,12 @@ for i, image in enumerate(images):
 	sexcatpath = os.path.join(alidir, image['imgname'] + ".alicat")
 		
 	# We run sextractor on the sky subtracted and aligned image :
-	command = "%s %s -c default_norm_template.sex -GAIN %.3f -PIXEL_SCALE %.3f -SATUR_LEVEL %.3f -CATALOG_NAME %s" % (sex, imgpath, image["gain"], image["pixsize"], image["satur_level"], sexcatpath)
-	
-	print command
-	os.system(command)
+	saturlevel = image["gain"] * image["saturlevel"] # to convert to electrons
+	cmd = "%s %s -c default_norm_template.sex -PIXEL_SCALE %.3f -SATUR_LEVEL %.3f -CATALOG_NAME %s" % (sex, imgpath, image["pixsize"], saturlevel, sexcatpath)
+	os.system(cmd)
+	print cmd
+
+	os.system(cmd)
 	
 	
 
