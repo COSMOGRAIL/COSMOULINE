8eeb1ad44373051ed8b068b2f76c4d7a8af84cc3
mtewes <mtewes@6d28bc59-9848-486d-8bae-1920113d1a71>
2011-03-01 16:04:53 +0000

    adding filter selection

--- trunk/pipe/0_preparation_scripts/SMARTS/crop_fix_filtercheck.py
+++ trunk/pipe/0_preparation_scripts/SMARTS/crop_fix_filtercheck.py
@@ -9,51 +9,51 @@ import os, sys, glob, pyfits, numpy
 ################### CONFIGURATION ###################################################################
 
 # ABSOLUTE PATH to where the files are and how to select them :
-origpaths = sorted(glob.glob("/obs/lenses_EPFL/SMARTS/J0158/SMARTS2008/*.fits")) 
+origpaths = sorted(glob.glob("/obs/lenses_EPFL/SMARTS/J0158/allsoftlinks/*.fits")) 
 
 # ABSOLUTE PATH to the directory where you want the croped images to be written :
-destdir="/obs/lenses_EPFL/SMARTS/J0158/SMARTS2008crop" 
+destdir="/obs/lenses_EPFL/SMARTS/J0158/allcrop" 
+
+
+#####################################################################################################
+
 
 def newpath(origpath, destdir): 	# specifies how to change the name :
 					# origpath is the full path to a present file, and you have to
 					# return a full path to the destination file you want.
-	
 	(dirname, filename) = os.path.split(origpath)
 	#newfilename = filename.replace(":", "-")
 	destpath = os.path.join(destdir, filename)
 	return destpath
 
-# pixels you want to keep (numpy style) :
-region = "[117:1035, 166:1020]"
-#region = None # If you do not want to crop
-
-#####################################################################################################
-
-
 
 if not os.path.isdir(destdir):
 	os.mkdir(destdir)
 
+filterlist = []
 
 for fitsfilepath in origpaths:
-	#print os.path.split(fitsfilepath)[1]
+	print os.path.split(fitsfilepath)[1]
 
 	newfitsfilepath = newpath(fitsfilepath, destdir)
 	
 	pixelarray, hdr = pyfits.getdata(fitsfilepath, header=True)
 	
-	if hdr["CCDFLTID"].strip() != "R":
-		print "2_%s\tFilter %s" % (os.path.splitext(os.path.split(fitsfilepath)[1])[0], hdr["CCDFLTID"])
+	filterid = hdr["CCDFLTID"].strip()
+	filterlist.append(filterid)
 	
-	"""
+	
+	if hdr["CCDFLTID"].strip() != "R":
+		continue
+		#print "2_%s\tFilter %s" % (os.path.splitext(os.path.split(fitsfilepath)[1])[0], hdr["CCDFLTID"])
+		
+	
 	pixelarray = numpy.asarray(pixelarray).transpose() # To put it in the usual ds9 orientation
 	
 	pixelarrayshape = pixelarray.shape
 	print "Input : (%i, %i), %s, %s" % (pixelarrayshape[0], pixelarrayshape[1], hdr["BITPIX"], pixelarray.dtype.name)
 	
-	
-	if region != None:
-		exec("pixelarray = pixelarray%s" % (region))
+	pixelarray = pixelarray[96:1037, 118:1024]
 	
 	pixelarrayshape = pixelarray.shape
 	print "Ouput : (%i, %i)" % (pixelarrayshape[0], pixelarrayshape[1])
@@ -69,4 +69,11 @@ for fitsfilepath in origpaths:
 	hdu = pyfits.PrimaryHDU(pixelarray.transpose(), hdr)
 	hdu.verify("fix")
 	hdu.writeto(newfitsfilepath)
-	"""
+
+	
+
+print "Filter histo :"
+filterhisto = [(f, filterlist.count(f)) for f in sorted(list(set(filterlist)))]
+print "\n".join(["%s : %i" % h for h in filterhisto])
+
+
