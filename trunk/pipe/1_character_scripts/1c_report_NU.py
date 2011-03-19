8f51099a517bc9aadc7f1a4563642a05633c678f
mtewes <mtewes@6d28bc59-9848-486d-8bae-1920113d1a71>
2011-03-19 12:48:43 +0000

    Minor changes

--- trunk/pipe/1_character_scripts/1c_report_NU.py
+++ trunk/pipe/1_character_scripts/1c_report_NU.py
@@ -1,15 +1,11 @@
-#
-#	write a report about the fishy things discovered in the analysis
-#
-
 execfile("../config.py")
 from kirbybase import KirbyBase, KBError
 from variousfct import *
 
 
-fields = ['datet', 'mhjd', 'imgname', 'telescopename', 'gogogo']
+fields = ['datet', 'mjd', 'imgname', 'telescopename', 'gogogo']
 db = KirbyBase()
-reporttxt = db.select(imgdb, ['recno'], ['*'], fields, sortFields=['mhjd'], returnType='report')
+reporttxt = db.select(imgdb, ['recno'], ['*'], fields, sortFields=['mjd'], returnType='report')
 
 reporttxtfile = open(os.path.join(workdir, "report_chronocontent.txt"), "w")
 reporttxtfile.write(reporttxt)
