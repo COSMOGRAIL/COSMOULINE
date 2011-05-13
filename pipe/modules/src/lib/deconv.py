2e216029104475b9fa0c0ba4cf3e9b810f7d5c28
mtewes <mtewes@users.noreply.github.com>
2011-05-13 08:07:47 +0000

    new pymcs

---
+++ pipe/modules/src/lib/deconv.py
@@ -0,0 +1,337 @@
+from PSF import *
+from Algorithms import *
+import utils as fn
+import numpy as np
+import time
+
+out = fn.Verbose()
+
+class Dec:
+    def __init__(self, images, noisemaps, psf, smoothing_psf, conv_fun, 
+                 img_shifts, smoothing, g_res, force_ini=False):
+        #Deconv parameters:
+        self.images = images
+        self.noisemaps = noisemaps
+        self.psf = psf
+        self.psf_sm = smoothing_psf
+        self.conv_fun = conv_fun
+        self.shifts = img_shifts
+        self.lambd = smoothing
+        self.g_res = g_res
+        
+        #Results:
+        self.model = None
+        self.last_res = None
+        self.ini = None
+        self.trace = []
+        
+        #Private parameters
+        self._sshape = psf.shape
+        self._bshape = images[0].shape
+        self._sfact = self._sshape[0]/self._bshape[0]
+        self._nb_img = len(images)
+        self._old_src_par = None
+        
+        #Initialization
+        self.set_ini()
+    
+    def set_ini(self):
+        ini = np.array([])
+        for i, im in enumerate(self.images):
+            ali = fn.shift(im, self.shifts[i][0]/self._sfact - self._get_offset(self.shifts[i][0]), 
+                           self.shifts[i][1]/self._sfact - self._get_offset(self.shifts[i][1]), 
+                           interp_order=3, mode='reflect')
+            ini = append(ini, fn.rebin(ali, self._sshape)/self._sfact**2.)
+        self.ini = mean(ini.reshape((len(self.images), self._sshape[0]*self._sshape[1])), 
+                          0).reshape(self._sshape)
+        
+    def get_im_resi(self, model_conv, im_nb):
+        convo = fn.shift(model_conv, -self.shifts[im_nb][0], -self.shifts[im_nb][1], 
+                       interp_order=3, mode='wrap')
+#        convo = self.conv_fun(self.psf, ali)
+        convo_m = fn.mean(convo, self._bshape[0], self._bshape[1])
+        resi = (self.images[im_nb] - convo_m)
+        err = fn.rebin(resi/self.noisemaps[im_nb], self._sshape)/self._sfact**2.
+        resi = fn.rebin(resi, self._sshape)/self._sfact**2.
+        ali_err = fn.shift(err, self.shifts[im_nb][0] - self._get_offset(self.shifts[im_nb][0]), 
+                          self.shifts[im_nb][1] - self._get_offset(self.shifts[im_nb][1]), 
+                          interp_order=3, mode='wrap')
+        ali_resi = fn.shift(err, self.shifts[im_nb][0] - self._get_offset(self.shifts[im_nb][0]), 
+                          self.shifts[im_nb][1] - self._get_offset(self.shifts[im_nb][1]), 
+                          interp_order=3, mode='wrap')
+        return ali_err, ali_resi
+    
+    def get_err(self, model, null):
+        _model = model.reshape(self._sshape)
+        err = np.zeros(self._sshape, dtype=float64)
+        _model_sm = self.conv_fun(self.psf_sm, _model)
+#        import scipy.ndimage.filters
+#        _model_sm = scipy.ndimage.filters.gaussian_laplace(_model, 3.)
+        khi_smooth = self.lambd*(_model - _model_sm)**2.
+        err += khi_smooth
+        _model_conv = self.conv_fun(self.psf, _model)
+        for i, im in enumerate(self.images):           
+            khi_fit = self.get_im_resi(_model_conv, i)[0]**2.
+            err += khi_fit
+        self.trace += [err.sum()]
+        return err.ravel()
+    
+    def deconv(self, it_nb, minstep_px=None, maxstep_px=None,  stepfact=None, radius=None):
+        out(2, 'Begin minimization procedure')
+        t = time.time()
+        self.set_ini()
+        minipar, lastpar = minimi(self.get_err, self.ini.ravel(),[], 
+                                  minstep_px=minstep_px, maxstep_px=maxstep_px, 
+                                  itnb=it_nb, stepfact=stepfact)
+        self.model, self.last_res = minipar[0].reshape(self._sshape), \
+                                    lastpar[0].reshape(self._sshape)
+        out(2, 'Done in', time.time()-t,'[s]')
+        return self.model.copy()
+        
+    def _get_offset(self, pos):
+        return 0
+        return (pos % self._sfact) - self._sfact/2.    
+    
+
+        
+class DecSrc(Dec):
+    def __init__(self, images, noisemaps, psf, smoothing_psf, conv_fun, img_shifts, smoothing, g_res,
+                 nb_src=0, src_ini=[], src_pad=5., src_range=None, force_ini=False, 
+                 max_iratio_range=None, maxpos_range=None):
+        Dec.__init__(images, noisemaps, psf, smoothing_psf, conv_fun, img_shifts, smoothing, g_res, force_ini=False)
+        #Source parameters:
+        self.nb_src = nb_src
+        self.src_ini = src_ini
+        self.src_pad = src_pad
+        self.src_range = src_range
+        self.force_ini = force_ini
+        self.max_iratio_range = max_iratio_range 
+        self.maxpos_range = maxpos_range
+        self.sources = [PSF(self._sshape, (self._sshape[0]/2., self._sshape[1]/2.)) 
+                        for i in xrange(self._nb_img)]
+        #Results:
+        self.model_src = PSF(self._sshape)
+        #Initialization
+        if self.nb_src:
+            self._set_ini_src()
+    
+    def get_err(self, model, srcpar):
+        _model = model.reshape(self._sshape)
+        srcerr = np.zeros(len(srcpar))
+        if self.nb_src:
+            srcerr = self._get_src_err(srcpar, _model)
+            self._old_src_par = srcpar.copy()
+            self._set_sources(srcpar, _model)
+        err = np.zeros(self._sshape, dtype=float64)
+        _model_sm = self.conv_fun(self.psf_sm, _model)
+        khi_smooth = self.lambd*(_model - _model_sm)**2.
+        err += khi_smooth
+        for i in xrange(self._nb_img):     
+            _model_conv = self.conv_fun(self.psf, _model+self.sources[i])
+            khi_fit = self.get_im_resi(_model_conv, i)[0]**2. 
+            err += khi_fit
+        self.trace += [err.sum()]
+        return append(err, srcerr)
+    
+    def _set_sources(self, srcpar, bkg):
+        for i in xrange(self._nb_img):
+            self._add_sources(self.sources[i], srcpar, i)
+    
+    def _add_sources(self, im, srcpar, im_ind):
+        im.reset()
+        for i in xrange(self.nb_src):
+            c1, c2, i0 =  srcpar[i*(2+self._nb_img)], srcpar[i*(2+self._nb_img)+1], srcpar[i*(2+self._nb_img)+2+im_ind]
+            im.addGaus_fnorm_trunc(self.g_res, c1, c2, i0)
+                
+    def _get_src_err(self, srcpar, bkg):
+        if self.max_iratio_range or self.maxpos_range:
+            err = np.zeros(len(srcpar))
+            #compute the error for each parameter
+            for i in xrange(self._nb_img):
+                #compute intensities errors
+                self.sources[i].reset()
+                for j in xrange(self.nb_src):
+                    p_k = j*(2+self._nb_img)+2+i
+                    #get the old values back
+                    param = self._old_src_par.copy()
+                    param[p_k] = srcpar[p_k]
+                    self._add_sources(self.sources[i], param, i)
+                    _model_conv = self.conv_fun(self.psf, self.sources[i] + bkg)
+                    err[p_k] = (self.get_im_resi(_model_conv, i)[0]**2.).sum()
+            for i in xrange(self.nb_src):
+                #compute centers errors
+                #change the parameter to evaluate
+                for j in xrange(2):
+                    p_k = i*(2+self._nb_img) + j
+                    #get the old values back
+                    param = self._old_src_par.copy()
+                    param[p_k] = srcpar[p_k]
+                    e = 0.
+                    for l in xrange(self._nb_img):
+                        self.sources[i].reset()
+                        self._add_sources(self.sources[l], param, l)
+                        _model_conv = self.conv_fun(self.psf, self.sources[l] + bkg)
+                        e += (self.get_im_resi(_model_conv, l)[0]**2.).sum()
+                    err[p_k] = e
+            #return the error list 
+            return abs(err)#-olderr.sum())
+        else:
+#            self._add_sources(srcpar, bkg)
+            return [0. for s in srcpar]
+        
+    def _set_ini_src(self):
+        import get_ini_par as init
+        import wsutils as ws
+        srcini =self.src_ini
+        force_ini = self.force_ini
+        try: 
+            bkgini = fn.get_data('bkg_ini.fits')
+            force_ini += (len(srcini)/3. != self.nb_src)
+        except:
+            force_ini = True
+        if force_ini:
+            out(3, 'Beginning initialization from scratch...')
+            srcpos, bkgini = init._get_ini(self.ini, self.psf, self.nb_src, self.g_res, self._sfact, 
+                                           self.src_range, self.conv_fun, div, self.src_pad)
+            ws.drop('INI_PAR', srcini)
+            fn.array2fits(bkgini, 'results/bkg_ini.fits')    
+            srcini = []
+            for i,p in enumerate(srcpos): 
+                if i==self.nb_src or self.nb_src==0: #tmp
+                    break
+                srcini += [p[0],p[1]]
+                for j in xrange(self._nb_img):
+                    srcini += [p[2]]
+        self.src_ini = np.array(srcini)
+        self._old_src_par = np.array(srcini)
+        self.ini = bkgini
+
+    
+class DecMC(Dec):
+    def deconv(self, it_nb, minstep_px=None, maxstep_px=None,  stepfact=None, radius=None):
+        out(2, 'Begin minimization procedure')
+        t = time.time()
+        self.set_ini()
+        minipar = self._minimi_MC(self.get_err, self.ini.ravel(), itnb=it_nb)
+        self.model = minipar.reshape(self._sshape)
+        out(2, 'Done in', time.time()-t,'[s]')
+        return self.model.copy()
+    
+    def _minimi_MC(self, func, param, itnb):
+        #TODO: implement sources support
+        nb_runs = 20
+        par = param.copy()*3.
+        std = par.std()/100.
+        lerr = (func(par,[])**2.).sum()
+        for i in xrange(nb_runs):
+            for j in xrange(itnb/nb_runs):
+                err = []
+                offs = []
+                for k in xrange(2):
+                    rdm = np.random.standard_normal(param.shape)
+                    offset = rdm*par/50. + std*np.sign(rdm)
+                    testpar = par + offset
+                    err += [(func(testpar,[])**2.).sum()]
+                    offs += [offset]
+                ind = np.argmin(np.array(err))
+                if err[ind] < lerr:
+                    lerr = err[ind]
+                    par += offs[ind]
+                    out(3, int(100*(itnb/nb_runs*i+j+1.)/itnb), '% done.', 
+                        'Error:', err[ind], '-r')
+    #            if (itnb/nb_runs*i+j+1.)/itnb == 1: par += offs[ind]
+            std *= 0.97
+        out(3)
+        return par
+
+    
+class DecPaste(Dec):
+    
+    def get_err(self, model, null):
+        _model = model.reshape(self._sshape)
+#        err = np.array(foreach(lambda i: self.get_im_resi(_model, i)[0], 
+#                               np.arange(len(self.images)), threads=len(self.images),
+#                               return_=True)).sum(0)
+#        err = np.array([self.get_im_resi(_model, i)[0] for i in xrange(self._nb_img)]).sum(0)
+        err = np.zeros(self._sshape, dtype=float64)
+        for i in xrange(self._nb_img):           
+            khi_fit = self.get_im_resi(_model, i)[0]
+            err += khi_fit
+        return err.ravel()
+    
+    def deconv(self, it_nb, minstep, maxstep, stepfact, radius=None):
+        #TODO: implement sources support
+        if radius is None: radius=2.*self._sshape[0]
+        if minstep is None or minstep == 0: minstep = 0.0001
+        if maxstep is None or maxstep == 0: maxstep = 0.05
+        minstep /= stepfact
+        maxstep /= stepfact
+        mask = fn.get_circ_mask(self._sshape, radius, 1., 0.)
+        model = np.zeros(self._sshape)
+        self.ini = model[:]#.copy()
+        err = self.get_err(model, []).reshape(self._sshape)
+        chi2 = (err**2.).sum()/self._nb_img**2.
+#        last_chi2 = chi2
+        last_pos = -1,-1
+        last_dir = -2
+#        last_err = np.copy(err)*0.99
+        convo = np.zeros(self._sshape, dtype=float64)
+        pos = np.unravel_index((np.abs(mask*err)).argmax(), err.shape)
+        step = (err[pos]/self._nb_img/self._sfact**2./self.psf.max()) /stepfact
+        psf = self.conv_fun(self.psf, self.psf_sm) 
+        psf /= psf
+        out(2, 'Begin minimization procedure')
+        t = time.time()
+        for i in xrange(it_nb):#*sshape[0]**2):
+            pos = np.unravel_index(np.abs(mask*err).argmax(), err.shape)
+            dir = (np.sign(err[pos]))
+            if pos == last_pos: 
+                if dir==last_dir:
+                    step *= 2.
+                else:
+                    step /= 4.
+            else:
+                step = (err[pos]/self._nb_img/self._sfact**2./self.psf.max()) /stepfact
+            step = np.clip(step, minstep, maxstep)
+#            step = min(step, maxstep)
+#            step = max(step, minstep)
+            last_dir = dir
+#            last_err = np.copy(err)
+            last_pos = pos
+#            last_chi2 = chi2
+            model[pos] += step*dir
+            self._paste_psf(convo, pos, step*dir) #TODO: test (doesn't work yet)
+#            convo = self.conv_fun(self.psf_sm, model)
+#            khi_smooth = self.lambd*np.abs(model - convo)
+            err = self.get_err(convo, []).reshape(self._sshape)#+khi_smooth
+            chi2 = (err**2.).sum()/self._nb_img**2.
+            self.trace += [chi2]
+            out(3, 'Iteration', i+1, '- Current pos:', pos, '- Error:', chi2, '-r')
+#            if chi2 > last_chi2:
+#                out(3)
+#                out(3, 'Increased error, exiting') 
+#                break
+        else:
+            out(3)
+        out(2, 'Done in', time.time()-t,'[s]')
+        convo = self.conv_fun(self.psf_sm, model)
+        self.model, self.last_res = convo, None
+        return self.model.copy()
+#        return convo, model
+    
+    def _paste_psf(self, dest, pos, val):
+        x, y = pos
+        size = self._sshape[0]
+        rad = size/2.
+        cr_x1 = max(x-rad, 0)
+        cr_x2 = min(x+rad, size)
+        cr_y1 = max(y-rad, 0)
+        cr_y2 = min(y+rad, size)
+        r_x1 = max(rad-x, 0)
+        r_x2 = min(rad+size-x, size)
+        r_y1 = max(rad-y, 0)
+        r_y2 = min(rad+size-y, size)
+        dest[cr_x1:cr_x2, cr_y1:cr_y2] += self.psf[r_x1:r_x2, r_y1:r_y2]*val
+    
+    
+    
---
+++ pipe/modules/src/lib/get_ini_par.py
@@ -0,0 +1,208 @@
+#!/usr/bin/python
+
+import utils as fn
+import wsutils as ws
+from PSF import *
+from AImage import *
+from wavelet import *
+import numpy
+from numpy import *
+import scipy.optimize
+import scipy.ndimage.interpolation
+#import psyco
+#psyco.full()
+
+out = fn.Verbose()
+CUDA = False
+
+def _get_ini_bkg(data, levels=4, conv_fn=None):
+#    wl = sepatrous(data, trifilter2d, levels=levels)
+    wl = atrous(data, trifilter2d, levels=levels, conv_fn= conv_fn)
+    bk =wl[-1] + wl[-2]# + wl[-3] + wl[-4]
+#    bk = wl[0] - (wl[1] + wl[2] + wl[3] + wl[4])
+    bk *= (bk>=0.) #no neg constraint
+    return bk
+    
+     
+def _get_ini(img, psf, nb_src, gres, sfact, 
+             src_range=None, convol=fn.conv, pad=5., fft_div=None):
+    data = img.copy()
+    resi_bk = img.copy()
+    refpar = 0., 0.
+    if fft_div:
+        out(3, 'FFT division...')
+        dec = fft_div(psf, data)
+        dec_bk = dec.copy()
+        ini_bkg = dec.copy()
+        
+        def __errfun(param):
+            resi = (dec - fn.gaussian(data.shape, param[0], param[1], param[2], param[3]))#/sig
+            resi *= (100000.*(param[3]<0.)+1.)
+            resi *= ((100. * (resi < 0.)) + 1.)
+            resi *= (100000.*(abs(param[1]-refpar[0])>2. or abs(param[2]-refpar[1])>2.) + 1.)
+            return (resi**2.).ravel()
+    else:
+        out(3, 'Wavelet decomposition...')
+        ini_bkg = _get_ini_bkg(data, conv_fn=convol)
+#        resi_bk -= ini_bkg
+        def __errfun(param):
+            resi = (data - convol(psf,fn.gaussian(data.shape, param[0], param[1], param[2], param[3])))#/sig
+            resi *= (100000.*(param[3]<0.)+1.)
+            resi *= ((100. * (resi < 0.)) + 1.)
+            resi *= (100000.*(abs(param[1]-refpar[0])>2. or abs(param[2]-refpar[1])>2.) + 1.)
+            return (resi**2.).ravel()
+    srcpos = array(fn.getGpos(resi_bk, nb_src, pad, cutoffs=src_range)[0])
+    ret = array([])
+    for i, p in enumerate(srcpos):
+        out(3, 'Getting initial parameters for source', i+1, '-r')
+        if fft_div:
+            xx, xy = maximum(p[0]-2.*pad, 0), minimum(p[0]+2.*pad, img.shape[0]-1)
+            yx, yy = maximum(p[1]-2.*pad, 0), minimum(p[1]+2.*pad, img.shape[1]-1)
+            data = resi_bk[xx:xy, yx:yy]
+            dec = dec_bk[xx:xy, yx:yy]
+            ini = [gres, 2.*pad, 2.*pad, resi_bk[p[0], p[1]]]
+            refpar = 2.*pad, 2.*pad
+        else:
+            data = resi_bk
+            ini = [gres, p[0], p[1], resi_bk[p[0], p[1]]]
+            refpar = p[0], p[1]
+        gini = scipy.optimize.leastsq(__errfun, ini)[0]
+        if fft_div:
+            gini[1] += xx
+            gini[2] += yx
+        resi_bk -= convol(psf,fn.gaussian(resi_bk.shape, gini[0], gini[1], gini[2], gini[3]))
+        if fft_div:
+            dec_bk -= fn.gaussian(dec_bk.shape, gini[0], gini[1], gini[2], gini[3])
+        gini[3] = gini[3]*gini[0]**2./gres**2.
+        ret = append(ret, gini[1:])
+    out(3, 'Done!')
+    if fft_div:
+        return ret.reshape((nb_src, 3)), dec_bk
+    return ret.reshape((nb_src, 3)), ini_bkg#resi_bk 
+        
+def set_ini(data, params):
+    #TODO: set to the new norms!! (params)
+    def X2x(X):
+        return X*sfact - 1./sfact
+    def x2X(x):
+        return x/sfact + 1./sfact**2.
+    nimg, sfact, gres = params['filenb'],params['S_FACT'], params['G_RES']
+    show, USE_CUDA, nb_src = params['SHOW'], params['CUDA'], params['NB_SRC']
+    box_size, src_range, fftdiv = params['BOX_SIZE'], params['SRC_RANGE'], params['FFT_DIV']
+    
+    obj = [data['objs'][i][0].astype('float64') for i in xrange(nimg)]
+    sigma = [data['objssig'][i][0].astype('float64') for i in xrange(nimg)]
+    psf = [data['psfs'][i][0].astype('float64') for i in xrange(nimg)]
+    bshape = obj[0].shape
+    sshape = (int(bshape[0]*sfact), int(bshape[1]*sfact))
+    
+    orig_fwhm = 4.5 # 4. #BIG pixels! ~3.
+    
+    if USE_CUDA and fn.has_cuda():
+        context, plan = fn.cuda_init(sshape)
+        out(2, 'CUDA initializations')
+        def convol(a, b):
+            return fn.cuda_conv(plan, a, b)
+        def deconv(a, b):
+            return fn.cuda_fftdiv(plan, a, b)
+        if not fftdiv:
+            deconv = None
+    else:
+        USE_CUDA = False
+        convol = fn.conv
+        deconv = None
+    
+    out(2, 'Interpolating data...')
+    obji = []
+    sigmai = []
+    flux_o = []
+    flux_s = []
+    for i in xrange(nimg):
+        d_filt = signal.signaltools.wiener(obj[i], [3,3])
+#        psf[i] = signal.signaltools.medfilt2d(psf[i], [3,3])
+#        psf[i] /= psf[i].sum()
+        obji += [scipy.ndimage.interpolation.zoom(d_filt, sfact, order = 5, mode='reflect').astype('float64')]
+        sigmai += [scipy.ndimage.interpolation.zoom(sigma[i], sfact, order = 5, mode='reflect').astype('float64')]
+        flux_o += [obj[i].sum()]
+        flux_s += [sigma[i].sum()]
+        obji[i] *= flux_o[-1]/obji[i].sum()
+        sigmai[i] *= flux_s[-1]/sigmai[i].sum()
+
+    out(2, 'Setting initial sources parameters...')
+    srcpos, ini = _get_ini(obji[0], sigmai[0], psf[0], nb_src, gres, sfact, nimg, orig_fwhm, 
+                           src_range, convol, deconv, pad=box_size)
+    
+    shiftpar = -fn.get_shifts(obj, boxsize=10)[1:]#array([0.1 for i in xrange(2*nimg)])
+    shiftpar = shiftpar.ravel()
+    
+    fn.array2fits(ini, 'results/bkg_ini.fits')    
+    ws.drop('INI_PAR', srcpos)
+    if show:
+        fn.array2ds9(obji[0], name='data')
+        fn.array2ds9(ini, name='background', frame=2)
+    if USE_CUDA:
+        out(2, 'Freeing CUDA context...')
+        context.pop()
+    return ini, srcpos, shiftpar
+    
+
+
+def main(argv=None):
+    cfg = 'config.py'
+    if argv is not None:
+        sys.argv = argv
+    opt, args = fn.get_args(sys.argv)
+    if args is not None: cfg = args[0]
+    if 's' in opt: 
+        out.level = 0
+    if 'v' in opt: 
+        out.level = 2
+    if 'd' in opt: 
+        DEBUG = True
+        out.level = 3
+        out(1, '~~~ DEBUG MODE ~~~')
+    if 'b' in opt: 
+        import prepare
+        prepare.main(['deconv.py', '-b', cfg])
+    if 'e' in opt: 
+        import prepare
+        prepare.main(['deconv.py', '-ce', cfg])
+    if 'h' in opt:
+        out(1, 'No help page yet!')
+        return 0
+    out(1, 'Begin initialization')
+    
+    #TODO: check workspace
+    CONV_IT = MAXPOS_STEP = MAX_IRATIO_STEP = SHOW = None
+    f = open(cfg, 'r')
+    exec f.read()
+    f.close()
+    vars = ['FILENAME', 'CONV_IT', 'S_FACT', 'G_RES', 'SIGMA_SKY',
+            'MOF_PARAMS', 'G_STRAT', 'G_PARAMS', 'G_POS', 'CENTER']
+    err = fn.check_namespace(vars, locals())
+    if err > 0:
+        return 1
+    out(2, FILENAME)
+    out(2, 'Restore data from extracted files')
+    fnb = ws.get_filenb(FILENAME)
+    files = ws.getfilenames(fnb)
+    data = ws.restore(*files)
+    data['filenb'] = fnb
+    set_ini(data, locals())
+#    if NOWRITE is False:
+#        fn.write_cfg(cfg, {'DEC_PAR':dec})
+    out(1, 'Initialization done')
+    
+    return 0
+    
+    
+if __name__ == "__main__":
+#    import cProfile, pstats
+#    prof = cProfile.Profile()
+#    prof = prof.runctx("main()", globals(), locals())
+#    stats = pstats.Stats(prof)
+#    stats.sort_stats("time")  # Or cumulative
+#    stats.print_stats(15)  # how many to print
+    sys.exit(main())
+
+
