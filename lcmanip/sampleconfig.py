f7037e07766a40f3a3b9f25d284e37562bf640d2
mtewes <mtewes@users.noreply.github.com>
2012-01-09 14:48:47 +0000

    working

--- lcmanip/sampleconfig.py
+++ lcmanip/sampleconfig.py
@@ -1,12 +1,35 @@
+# This file configures lcmanip.
+# 1) copy this sampleconfig.py into config.py
+# 2) Set lcmanipdir below
+#    This is the path to the directory containing the exported cosmouline pkl database that
+#    you want to manipulate. By default, this would be cosmoulines configdir, as the pkl
+#    is saved there.
+# 3) Copy samplelcmanip.py as lcmanip.py into the lcmanipdir. 
+# 4) All further settings are done in this lcmanip.py
 
-pkldbpath = "~/Desktop/2011-02-15_WFI2033_C2_db.pkl"
+lcmanipdir = "/Users/mtewes/Desktop/f_Q1355_C2"
 
 
 
+# Normally you do not have to change anything below this line.
+################################################################################################
 
 import os
 import shutil
 import sys
 import numpy as np
+#sys.path.append("../modules")
+sys.path.append("modules")
 
-sys.path.append("../modules")
+import groupfct
+import variousfct
+import rdbexport
+
+
+
+execfile(os.path.join(lcmanipdir, "lcmanip.py"))
+
+print "    ### Working on %s ###" % dbfilename
+
+
+
---
+++ lcmanip/samplelcmanip.py
@@ -0,0 +1,21 @@
+
+# This is a "settings" file for lcmanip
+# It contains the configuration that will be used to convert the
+# exported cosmouline database pkl into an rdb file ready for pycs.
+
+
+# Filename of the exported cosmouline db :
+dbfilename = "2012-01-09_f_Q1355_C2_db.pkl"
+
+
+# Name of the deconvolution (see the README file that comes with the db ...)
+deconvname = "dec_wowfixback_lens_medcoeff_pyMCSabcdf1"
+
+# Name of the pointsources of this deconvolution
+sourcenames = ["A", "B"]
+
+# Name of the normalization coeff to use :
+normcoeffname = "renormabcde"
+
+
+
