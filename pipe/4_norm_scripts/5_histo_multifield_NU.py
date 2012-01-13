94d85636951967c6d600fbc0cd17b951370ebb73
mtewes <mtewes@users.noreply.github.com>
2012-01-13 10:49:46 +0000

    Fixed ranges

--- pipe/4_norm_scripts/5_histo_multifield_NU.py
+++ pipe/4_norm_scripts/5_histo_multifield_NU.py
@@ -19,27 +19,37 @@ import matplotlib.pyplot as plt
 db = KirbyBase()
 allimages = db.select(imgdb, ["gogogo"], [True], returnType='dict', sortFields=['mjd'])
 	
-for fieldname in ["medcoeff", "skylevel", "seeing", "ell"]:
+for (fieldname, plotrange) in [("medcoeff", [0.5, 3.0]), ("skylevel", [0.0, 10000.0]), ("seeing", [0.5, 3.0]), ("ell", [0.0, 0.5])]:
 
 
 	images = [image for image in allimages if image[fieldname] > 0.0]
 	print "%s : %i images" % (fieldname, len(images))
 	usedsetnames = sorted(list(set([image['setname'] for image in images])))
 
-	minval = np.min(np.array([image[fieldname] for image in images]))
-	maxval = np.max(np.array([image[fieldname] for image in images]))
-	print "min, max = %f, %f" % (minval, maxval)
-
+	#minval = np.min(np.array([image[fieldname] for image in images]))
+	#maxval = np.max(np.array([image[fieldname] for image in images]))
+	#minmaxtext = "min, max = %f, %f" % (minval, maxval)
+	#print minmaxtext
+	
 	for usedsetname in usedsetnames :
 	
-		#print usedsetname
+		print "Set ", usedsetname
 		setimages = [image for image in images if image["setname"] == usedsetname]
+		
+		setminval = np.min(np.array([image[fieldname] for image in setimages]))
+		setmaxval = np.max(np.array([image[fieldname] for image in setimages]))
+		setminmaxtext = "min, max = %f, %f" % (setminval, setmaxval)
+		print setminmaxtext
+	
 		telescopenames = sorted(list(set([image['telescopename'] for image in setimages])))
 		values = np.array([image[fieldname] for image in setimages])
 		plt.figure(figsize=(15,5))
 	
-		plt.hist(values, 500, range=(minval, maxval), facecolor='red', edgecolor="none", log=False)
-		plt.xlabel("Field : %s" % (fieldname))
+		#plt.hist(values, 500, range=(minval, maxval), facecolor='red', edgecolor="none", log=False)
+		plt.hist(values, 300, range=(plotrange[0], plotrange[1]), facecolor='red', edgecolor="none", log=False)
+		plt.xlim(plotrange[0], plotrange[1])
+		
+		plt.xlabel("Field : %s       (%s)" % (fieldname, setminmaxtext))
 		plt.title("Setname : %s (Telescopes : %s), %i images." % (usedsetname, "+".join(telescopenames), len(setimages)))
 	
 		if savefigs:
