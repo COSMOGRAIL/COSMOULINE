051d9e21459a399ab5311ecee5230b50e724e454
mtewes <mtewes@users.noreply.github.com>
2012-01-28 15:09:30 +0000

    Adding some more precise float-formatting to the exported rdb file.

--- lcmanip/modules/rdbexport.py
+++ lcmanip/modules/rdbexport.py
@@ -9,13 +9,18 @@
 from variousfct import *
 
 
-def writerdb(columns, filename, writeheader=True):
+def writerdb(columns, filename, writeheader=True, autoformat=True):
+	"""
+	If autoformat is True, I'll format some common columns a bit nicer.
+	"""
 	# columns = [{"name":string, "data":pythonlist}, ...], ordered as you want them to be in the rdb file.
 	# filename = full path to the file to write.
 	
 	import csv
 	import sys
 	import os
+	import copy
+	
 	
 	# Just to be sure, we stat by checking if the columns all have the same length.
 	lengths = map(lambda x : len(x["data"]), columns)
@@ -27,6 +32,27 @@ def writerdb(columns, filename, writeheader=True):
 	if 0 in map(len, colnames):
 		raise mterror("C'mon, give a name to that poor column !")
 	
+	# Tests done.
+	
+	# We make a copy, as we might change the columns content :
+	columns = copy.deepcopy(columns)
+	#for column = columns:
+	#	column["data"] = column["data"]
+	
+	if autoformat:
+		for column in columns:
+			# Stuff with high precision :
+			if column["name"] in ["mhjd"] or "mag_" in column["name"]:
+				column["data"] = map(lambda x: "%.6f" % (x), column["data"])
+			elif "magerr_" in column["name"]:
+				column["data"] = map(lambda x: "%.4f" % (x), column["data"])
+			# Stuff with medium precision :
+			elif column["name"] in ["fwhm", "elongation", "airmass", "normcoeff"]:
+				column["data"] = map(lambda x: "%.3f" % (x), column["data"])
+			# Stuff with low precision :
+			elif column["name"] in ["relskylevel"]:
+				column["data"] = map(lambda x: "%6.1f" % (x), column["data"])
+
 	
 	underline = ["="*n for n in map(len, colnames)]	
 	data = zip(*[column["data"] for column in columns])
