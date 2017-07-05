d465a38d8cfddf4c570b6294584459f8f8a35dd0
vbonvin <vivien.bonvin@epfl.ch>
2017-07-05 14:23:37 +0200

    Cleaning and adding a few important files (license, readme, ...)

--- pipe/progs/oldpsfMCSf77/param_psf.f
+++
@@ -1,4 +0,0 @@
-       integer nbr1,nbr2,xy1,xy2,nbg
-c       parameter(nbr1=  1024,nbr2=  4096,xy1=  32,xy2=  64,nbg=  10)
-       parameter(nbr1=  4096,nbr2=  16384,xy1=  64,xy2=  128,nbg=  5)
-c nbg est le nombre d'etoiles !!!!
