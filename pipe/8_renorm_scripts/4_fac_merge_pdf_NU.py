4fb2634304ff56af38872ad59db7865deab70199
Frédéric Dux <duxfrederic@gmail.com>
2022-05-28 18:58:41 +0200

    another miss

--- pipe/8_renorm_scripts/4_fac_merge_pdf_NU.py
+++ pipe/8_renorm_scripts/4_fac_merge_pdf_NU.py
@@ -1,7 +1,17 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import glob
-exec(compile(open("../config.py", "rb").read(), "../config.py", 'exec'))
-from variousfct import *
+import sys
+import os
+if sys.path[0]:
+    # if ran as a script, append the parent dir to the path
+    sys.path.append(os.path.dirname(sys.path[0]))
+else:
+    # if ran interactively, append the parent manually as sys.path[0] 
+    # will be emtpy.
+    sys.path.append('..')
+from config import plotdir, settings
+
+renormname = settings['renormname']
 
 files = sorted(glob.glob(plotdir + "renorm_" + renormname +"*.pdf"))
 print("I will create a single pdf file with all the renormalisation plots") 
@@ -22,11 +32,4 @@ for fil in files :
 # Writing all the collected pages to a file
 output.write(open(plotdir + "Combined_"+renormname+".pdf","wb"))
 
-if computer == "regor4":
-	print("I can copy it in your visudir if you want.") 
-	proquest(askquestions)
-	
-	if not os.path.exists(visudir + "/plots"): 
-		os.makedirs(visudir + "/plots")
 
-	os.system("cp "+plotdir+"Combined_"+renormname+".pdf " + visudir + "/plots")
