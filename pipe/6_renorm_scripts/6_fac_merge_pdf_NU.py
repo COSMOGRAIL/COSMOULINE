b18a52fb7e56e0309ec889003187ba4f52829bdb
Frédéric Dux <duxfrederic@gmail.com>
2023-05-23 09:27:44 +0200

    updated pypdf2 usage w api changes

--- pipe/6_renorm_scripts/6_fac_merge_pdf_NU.py
+++ pipe/6_renorm_scripts/6_fac_merge_pdf_NU.py
@@ -1,4 +1,4 @@
-from PyPDF2 import PdfFileWriter, PdfFileReader
+from PyPDF2 import PdfWriter, PdfReader
 import glob
 import sys
 import os
@@ -21,15 +21,15 @@ for setname in setnames:
 
     # Creating a routine that appends files to the output file
     def append_pdf(inp,out):
-        [output.addPage(inp.getPage(page_num)) for page_num in range(inp.numPages)]
+        [output.add_page(inp.pages[page_num]) for page_num in range(len(inp.pages))]
 
     # Creating an object where pdf pages are appended to
-    output = PdfFileWriter()
+    output = PdfWriter()
 
     # Appending two pdf-pages from two different files
 
     for fil in files : 
-        append_pdf(PdfFileReader(open(fil,"rb")),output)
+        append_pdf(PdfReader(open(fil,"rb")),output)
         
 
     # Writing all the collected pages to a file
